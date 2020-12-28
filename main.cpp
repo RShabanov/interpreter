3eb141bf739d43e19ed51164683b7b0d404957ed
ShabanovR <55501178+ShabanovR@users.noreply.github.com>
2020-12-28 20:01:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,17 @@
 #include <iostream>
-#include <cstdlib>
 #include "Parser.h"
 using namespace std;
 
 
-class Exception {
-public:
-	void msg() {
-		cout << "Something went wrong!\n";
-	}
-
-	friend ostream& operator<<(ostream& out, const Exception& e) {
-		out << "Something went wrong!\n";
-		return out;
-	}
-};
-
-void foo(int t) {
-	cout << "Foo!\n";
-	if (t == 2) throw Exception();
-}
-
-void coo(int t) {
-	foo(t);
-	cout << "Coo!\n";
-}
-
-void boo(int t) {
-	coo(t);
-	cout << "Boo!\n";
-}
-
-
-struct A
-{
-	int a;
-};
 
 int main() {
 
-	/*for (int i = 0; i < 5; i++) {
-		cout << "i = " << i << endl;
-		try {
-			boo(i);
-		}
-		catch (Exception& e) {
-			cout << e;
-			continue;
-		}
-		cout << "i = " << i << endl;
-	}*/
+	string str = "5+(4-3)*2";
 
-	A a;
-	a.a = 50;
+	Parser parser(&str[0]);
 
-	const A* ptra = &a;
-
-
+	double res = 0;
+	parser.parse(res);
 	cin.get();
 	return 0;
 }
